ffe6496d1d0c65149b32fa86e65c9b67ab870e3a
Sakdipat Ontoum <sakdipat3536@gmail.com>
2020-04-11 16:59:56 +0700

    [ADD] fire-base setup and do simple sign-in anonymous

---
+++ lib/src/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:lxnavigator/src/aunthenticate/aunthenicate.dart';
+import 'package:lxnavigator/src/home/view/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    //return either Home and Aunthenicate widget
+    return Authenticate();
+    
+  }
+}
