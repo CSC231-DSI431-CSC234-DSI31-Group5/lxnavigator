02b3b72d09c6a626cb0c8d0b668fbb34d487c26f
Sakdipat Ontoum <sakdipat3536@gmail.com>
2020-05-03 01:18:25 +0700

    [EDIT] [create database for user collection for send and fetch to firebase.

--- lib/src/home/views/home.dart
+++ lib/src/home/views/home.dart
@@ -50,92 +50,6 @@ class Home extends StatelessWidget {
   }
 }
 
-// class DrawerCode extends StatefulWidget {
-//   @override
-//   _DrawerCodeState createState() => _DrawerCodeState();
-// }
-
-// class _DrawerCodeState extends State<DrawerCode> {
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
-//   FirebaseUser user;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     initUser();
-//   }
-
-//   initUser() async {
-//     user = await _auth.currentUser();
-//     setState(() {});
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Drawer(
-//       child: Container(
-//         child: ListView(
-//           padding: EdgeInsets.zero,
-//           children: <Widget>[
-//             DrawerHeader(
-//               decoration: BoxDecoration(color: Colors.white),
-//               child: Column(
-//                 children: <Widget>[
-//                   Flexible(
-//                     child: Container(
-//                       padding:
-//                           EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-//                       width: 100,
-//                       height: 100,
-//                       margin: EdgeInsets.only(bottom: 5),
-//                       decoration: BoxDecoration(
-//                         shape: BoxShape.circle,
-//                         image: DecorationImage(
-//                             image: AssetImage('assets/images/avatar/profile.png'),
-//                             fit: BoxFit.fill),
-//                       ),
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//             Container(
-
-//               child: ListTile(
-//                 // leading: Icon(Icons.person_outline),
-//                 title: Text("Name \n\n Sakdipat Ontoum \n\n "),
-//               ),
-//             ),
-//             Container(
-//               child: ListTile(
-//                 // leading: Icon(Icons.settings_applications),
-//                 title: Text("Email \n\n ${user?.email} \n\n "),
-//               ),
-//             ),
-//             Container(
-//               child: ListTile(
-//                 // leading: Icon(Icons.arrow_back),
-//                 title: Text("Occupation \n\n Student \n\n"),
-//               ),
-//             ),
-//             Container(
-//               child: RaisedButton(
-//                   padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-//                   color: Colors.orange[400],
-//                   child: Text(
-//                     'Sign Out',
-//                     style: TextStyle(color: Colors.white),
-//                   ),
-//                   onPressed: () async {
-//                     await _auth.signOut();
-//                   }),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 class DrawerCode extends StatefulWidget {
   @override
@@ -247,14 +161,3 @@ class _DrawerCodeState extends State<DrawerCode> {
     );
   }
 }
-
-// // title: Text(uid==user.uid ? "name \n\n" + firstname.toString()+" "+lastname.toString() + "\n\n" : " "),
-//       );
-//  ListTile(
-//   // leading: Icon(Icons.settings_applications),
-//   title: Text("Email \n\n ${user?.email} \n\n "),
-// ),
-// ListTile(
-//   // leading: Icon(Icons.arrow_back),
-//   title: Text(uid==user.uid ? "occupation \n\n" + occupation.toString()+"\n\n": " " ),
-// );
